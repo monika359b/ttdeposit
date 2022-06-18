55f6f9d6f6a58d962937ff8729f2b46ea4973a84
monika359b <89681654+monika359b@users.noreply.github.com>
2022-06-18 07:46:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ console.log(ba)
             .then((data) => {
                 res.status(200).json(data)
         });
-} catch (e) {
+} finally catch (e) {
         console.error(e);
         res.status(404).json({
             message : 'Transaction Failed'})
